2aa836d098646ba750faa3ccc9d687e4d173ee4b
Annabelle Perbet <annabelleperbet@yahoo.fr>
2025-05-15 17:06:26 +0200

    Création de la fonction reverseString

---
+++ script.js
@@ -0,0 +1,10 @@
+// Exercice JS : reverse a string
+
+const reverseString = (str) => {
+  return str.split("").reverse().join("");
+};
+
+console.log(reverseString(".srevne'l à etircé esarhp enu tse icec"));
+
+// Deuxième façon
+//const reverseString = (str) => str.split("").reverse().join("");
